dbcce2634a831d145bcf42a0099b30796020e6fd
micaldas <micaldas@mailfence.com>
2023-07-16 16:41:22 +0100

    16-07-2023 commit

--- auto/tasks.py
+++ auto/tasks.py
@@ -1,13 +1,21 @@
 """
 Updates git repos in the python and web folders.
 """
+
+import contextlib
 import os
 import subprocess
 from datetime import datetime
 from time import sleep
 
 import requests
-import snoop
+
+# import snoop
+# from snoop import pp
+
+
+# def type_watch(source, value):
+#     return f"type({source})", type(value)
 
 
 # @snoop
@@ -18,9 +26,11 @@ def update():
     is not present.
     """
     fullpaths = []
-    py = os.listdir("/home/mic/python/")
+    ignored = ["reusable_files", "old_alternative_projects"]
+    py = [i for i in os.listdir("/home/mic/python/") if i not in ignored]
     site = os.listdir("/usr/share/nginx/html/")
-    clean_site = [i for i in site if i != "setup.cfg"]
+    omit = ["setup.cfg", "50x.html", "index.html"]
+    clean_site = [i for i in site if i not in omit]
 
     for i in py:
         fpath = os.path.join("/home/mic/python", i)
@@ -28,35 +38,25 @@ def update():
     for f in clean_site:
         fpath = os.path.join("/usr/share/nginx/html/", f)
         fullpaths.append(fpath)
-    print(fullpaths)
 
+    add = "git add ."
+    push_github = "git push -f origin_github HEAD"
     for path in fullpaths:
         os.chdir(path)
-        date = datetime.today().strftime("%d-%m-%Y")
-        add = "git add ."
+        date = datetime.now().strftime("%d-%m-%Y")
         commit = f"git commit -m '{date} commit'"
-        push_github = "git push -u origin_github master"
-        push_notabug = "git push -u origin master"
-        with open(f"{path}/.git/config", "r") as f:
-            git_remotes = f.readlines()
-            gr = str(git_remotes)
-            if '[remote "origin"]' in gr:
-                subprocess.run(add, cwd=path, shell=True)
-                sleep(1)
-                subprocess.run(commit, cwd=path, shell=True)
-                sleep(1)
-                subprocess.run(push_notabug, cwd=path, shell=True)
-                sleep(1)
-            else:
-                print(f"There's no repo notabug in {path}")
-            if '[remote "origin_github"]' in gr:
-                subprocess.run(add, cwd=path, shell=True)
-                sleep(1)
-                subprocess.run(commit, cwd=path, shell=True)
-                sleep(1)
-                subprocess.run(push_github, cwd=path, shell=True)
-            else:
-                print(f"There's no repo github in {path}")
+        with contextlib.suppress(FileNotFoundError):
+            with open(f"{path}/.git/config", "r") as f:
+                git_remotes = f.readlines()
+                gr = str(git_remotes)
+                if '[remote "origin_github"]' in gr:
+                    subprocess.run(add, cwd=path, shell=True)
+                    sleep(1)
+                    subprocess.run(commit, cwd=path, shell=True)
+                    sleep(1)
+                    subprocess.run(push_github, cwd=path, shell=True)
+                else:
+                    print(f"There's no ref origin_github in {path}")
 
 
 if __name__ == "__main__":
@@ -68,8 +68,10 @@ def notify():
     Sends notifcation to phone that the
     script has been run.
     """
-    requests.post('https://ntfy.sh/mic', data='Git update has been run. '.encode(encoding='utf-8'))
+    requests.post(
+        "https://ntfy.sh/mic", data="Git update has been run. ".encode(encoding="utf-8")
+    )
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     notify()
